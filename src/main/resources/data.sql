ca94cddf29de5607bb15ff895899c9a857bbbf6f
luorijun <luorijun@outlook.com>
2021-03-03 18:34:13 +0800

    增加初始化商品的库存

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- 通过 ddl 生成的数据表，错误可以忽略
 
 insert into shopgoods (name, price, stock)
-values ('商品一', 15.5, 20);
+values ('商品一', 15.5, 100);
 
 insert into shopgoods (name, price, stock)
-values ('商品二', 18.8, 30);
+values ('商品二', 18.8, 200);
 
 insert into shopgoods (name, price, stock)
-values ('商品一', 99.9, 10);
+values ('商品一', 99.9, 50);
