d87999826eb92bd304b78cc8b0d11ff6fb788339
luorijun <luorijun@outlook.com>
2021-03-03 12:44:21 +0800

    添加前端依赖，使用 thymeleaf

--- src/main/java/luorijun/learn/springdataredis/controllers/IdleController.java
+++ src/main/java/luorijun/learn/springdataredis/controllers/IdleController.java
@@ -1,13 +1,13 @@
 package luorijun.learn.springdataredis.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IdleController {
 
     @GetMapping
     public String idle() {
-        return "Hello World";
+        return "index";
     }
 }
