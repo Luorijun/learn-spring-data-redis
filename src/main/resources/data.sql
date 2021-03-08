c05f0affb50e2503992bd2114a4b712aac8abe99
luorijun <luorijun@outlook.com>
2021-03-09 00:49:02 +0800

    运行时初始化数据

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into shop_goods (name, price, stock)
+values ('商品1', 16.6, 100);
+
+insert into shop_goods (name, price, stock)
+values ('商品2', 28.8, 200);
+
+insert into shop_goods (name, price, stock)
+values ('商品3', 39.9, 50);
