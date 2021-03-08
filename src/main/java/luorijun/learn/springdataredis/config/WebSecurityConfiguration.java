df3a373bbf3b299fa092cf16d8a88941d4edbc0d
luorijun <luorijun@outlook.com>
2021-03-08 23:16:48 +0800

    修改安全配置，删除首页

--- src/main/java/luorijun/learn/springdataredis/config/WebSecurityConfiguration.java
+++ src/main/java/luorijun/learn/springdataredis/config/WebSecurityConfiguration.java
@@ -5,34 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder encoder;
 
-    public WebSecurityConfiguration(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
+    public WebSecurityConfiguration(PasswordEncoder encoder) {
+        this.encoder = encoder;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/api/**", "/h2-console/**");
-        http.headers().frameOptions().sameOrigin();
+        http.formLogin();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        var user = User
-                .withUsername("user")
-                .password(passwordEncoder.encode("user"))
-                .authorities("USER");
-
-        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
-                .withUser(user);
+        var config = auth.inMemoryAuthentication();
+        config.passwordEncoder(encoder);
+        config.withUser("user").password(encoder.encode("user")).authorities("USER");
     }
 
     @Bean
--- src/main/java/luorijun/learn/springdataredis/controllers/IdleController.java
+++ src/main/java/luorijun/learn/springdataredis/controllers/IdleController.java
@@ -3,13 +3,16 @@ package luorijun.learn.springdataredis.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping
 public class IdleController {
 
     @GetMapping
+
+    @ResponseBody
     public String idle() {
-        return "index";
+        return "Idle Page";
     }
 }
